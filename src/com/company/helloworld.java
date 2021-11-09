834d3d4af858fb5ce4cc282126a534abad2988f1
is147groupproject <93957009+is147groupproject@users.noreply.github.com>
2021-11-08 21:06:24 -0500

    Update helloworld.java

--- src/com/company/helloworld.java
+++ src/com/company/helloworld.java
@@ -2,5 +2,6 @@ package com.company;
 public class helloworld {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("testing");
     }
 }
